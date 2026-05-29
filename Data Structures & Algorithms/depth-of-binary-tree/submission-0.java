3fbfcc29b3d2bcc5758647422ca92717259dfa73
sruthisureshkumar-arch <sruthisureshkumar@gmail.com>
2026-05-29 11:25:02 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int left_max =0;
+    int right_max = 0;
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+         int left = maxDepth(root.left);
+        
+        
+            int right = maxDepth(root.right);
+        
+        return left>right ? left +1 : right+1;
+        
+    }
+}
